976e2ea837fa2968a12f8ccaf0e7436a9cfb619b
Creed-Creed <97214510+Creed-Creed@users.noreply.github.com>
2022-01-13 15:25:13 +0530

    Initial commit

---
+++ mySite.py
@@ -0,0 +1,34 @@
+
+
+from flask import Flask,render_template,Response
+from cv2 import cv2
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def camera():
+    return render_template('camera.html')
+
+def get_frame():
+    cam = cv2.VideoCapture(0)
+    c = 0
+
+    while True:
+        ret,img=cam.read()
+        if c % 8 == 0:
+            cv2.imwrite("{}.jpg".format(c), img)
+        c += 1
+        imgencode=cv2.imencode('.jpg',img)[1]
+        stringData = imgencode.tostring()
+        
+        yield (b'--frame\r\n'
+        b'Content-Type: Text/plain\r\n\r\n' + stringData+b'\r\n')
+    
+
+@app.route('/video_stream')
+def video_stream():
+    return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
